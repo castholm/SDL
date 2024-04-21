d334a972a3df456be2a89c732d4d3269c30a3bb1
Carl Åstholm <carl@astholm.se>
2024-04-22 00:47:05 +0200

    zig: Linux

--- src/dialog/unix/SDL_zenitydialog.c
+++ src/dialog/unix/SDL_zenitydialog.c
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 #include "SDL_internal.h"
-#include "./SDL_dialog.h"
+#include <SDL3/SDL_dialog.h>
 
 #include <errno.h>
 #include <sys/types.h>
--- src/video/SDL_vulkan_internal.h
+++ src/video/SDL_vulkan_internal.h
@@ -49,7 +49,7 @@
 #endif
 
 #define VK_NO_PROTOTYPES
-#include "./khronos/vulkan/vulkan.h"
+#include <vulkan/vulkan.h>
 
 #include <SDL3/SDL_vulkan.h>
 
